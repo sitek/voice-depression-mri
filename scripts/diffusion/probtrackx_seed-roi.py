6d6efb6056c35fd6669520b8d7313c164a7206f6
sitek <sitek.kevin@gmail.com>
2017-07-28 17:15:53 -0400

    fixed workflow connections

--- scripts/diffusion/probtrackx_seed-roi.py
+++ scripts/diffusion/probtrackx_seed-roi.py
@@ -1,5 +1,4 @@
 
-import numpy as np
 import nipype.interfaces.io as nio
 import nipype.interfaces.fsl as fsl
 import nipype.interfaces.utility as util
@@ -13,7 +12,6 @@ info = dict(dwi=[['subject_id', 'data']],
             thsamples=[['subject_id', 'merged_th1samples']],
             fsamples=[['subject_id', 'merged_f1samples']],
             phsamples=[['subject_id', 'merged_ph1samples']],
-#            seed_file=[['subject_id', 'MASK_average_thal_right']],
             target_masks=[['subject_id', ['nodif_brain_mask']]])
 
 infosource = pe.Node(interface=util.IdentityInterface(fields=['subject_id']),
@@ -23,11 +21,8 @@ infosource.iterables = ('subject_id', subject_list)
 datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'],
                                                outfields=list(info.keys())),
                      name='datasource')
-
 datasource.inputs.template = "%s/%s"
-
 datasource.inputs.base_directory = os.path.abspath('/om/project/voice/processedData/tracula/')
-
 datasource.inputs.field_template = dict(dwi='%s/dmri/%s.nii.gz',
                                         aseg='%s/dlabel/diff/%s.bbr.nii.gz',
                                         thsamples='%s/dmri.bedpostX/%s.nii.gz',
@@ -53,22 +48,26 @@ datasink.inputs.base_directory = os.path.abspath('/om/scratch/Fri/ksitek/probtra
 
 tractography = pe.Workflow(name='tractography')
 tractography.connect([
-    (infosource, datasource, [('subject_id', 'subject_id')]),
-    (datasource, binarize, [('aparc','in_file')]) ])
+    (infosource, datasource, [('subject_id', 'subject_id')],),
+    (datasource, binarize, [('aparc','in_file'),
+                            ('thsamples', 'thsamples'),
+                            ('phsamples', 'phsamples'),
+                            ('fsamples', 'fsamples') ])])
 tractography.connect([(binarize, probtrackx, [('binary_file', 'seed')]
 tractography.connect([(datasource, probtrackx,
                       [('thsamples', 'thsamples'),
                        ('phsamples', 'phsamples'),
-                       ('samples', 'fsamples')
-                       ('target_masks', 'mask') ])])
+                       ('fsamples', 'fsamples'),
+                       ('target_masks', 'mask'),
+                       ('aseg','aseg') ])])
 tractography.connect([(infosource, datasink, [('subject_id', 'container')]),
                       (probtrackx, datasink,
-                      [('probtrackx.fdt_paths','probtrackx.@fdt_paths')],
-                      [('probtrackx.log','probtrackx.@log')],
-                      [('probtrackx.lookup_tractspace','probtrackx.@lookup_tractspace')],
-                      [('probtrackx.matrix2_dot','probtrackx.@matrix2_dot')],
-                      [('probtrackx.targets','probtrackx.@targets')],
-                      [('probtrackx.way_total','probtrackx.@way_total')] ) ])
+                      [('probtrackx.fdt_paths','probtrackx.@fdt_paths'),
+                       ('probtrackx.log','probtrackx.@log'),
+                       ('probtrackx.lookup_tractspace','probtrackx.@lookup_tractspace'),
+                       ('probtrackx.matrix2_dot','probtrackx.@matrix2_dot'),
+                       ('probtrackx.targets','probtrackx.@targets'),
+                       ('probtrackx.way_total','probtrackx.@way_total') ) ])
 
 tractography.run('SLURM',
                  plugin_args={'sbatch_args': '--qos=gablab --time=18:00:00 -N1 -c2 --mem=40G'})
