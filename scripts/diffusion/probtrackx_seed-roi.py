7e354f57ae7b5d849ea5862e0f3e8b7e3b5df14c
sitek <sitek.kevin@gmail.com>
2017-07-28 17:37:40 -0400

    fixed workflow connections

--- scripts/diffusion/probtrackx_seed-roi.py
+++ scripts/diffusion/probtrackx_seed-roi.py
@@ -1,4 +1,9 @@
+# Run probabilistic tractography via FSL FDT's ProbTrackX2
+# Requires having run bedpostx (or TRACULA) already
+# Also extracts an aparc seed mask
+# KRS 2017.07.28
 
+import os
 import nipype.interfaces.io as nio
 import nipype.interfaces.fsl as fsl
 import nipype.interfaces.utility as util
@@ -8,7 +13,7 @@ import nipype.interfaces.freesurfer as fs
 subject_list = ['voice999']
 
 info = dict(dwi=[['subject_id', 'data']],
-            aseg=[['subject_id', 'aparc+aseg+2mm']]
+            aseg=[['subject_id', 'aparc+aseg+2mm']],
             thsamples=[['subject_id', 'merged_th1samples']],
             fsamples=[['subject_id', 'merged_f1samples']],
             phsamples=[['subject_id', 'merged_ph1samples']],
@@ -34,11 +39,13 @@ datasource.inputs.template_args = info
 datasource.inputs.sort_filelist = True
 
 binarize = pe.Node(interface=fs.model.Binarize(),name='binarize')
-binarize.inputs.match = 4035 # wm-rh-insula
+binarize.inputs.match = [4035] # wm-rh-insula
 
 pbx2 = pe.Node(interface=fsl.ProbTrackX2(), name='probtrackx')
 pbx2.inputs.out_dir = os.path.abspath('/om/project/voice/processedData/probtrackx2')
-pbx2.inputs.omatrix2=True
+pbx2.inputs.target2 = os.path.join('/om/project/voice/processedData/tracula/',
+                                    'voice999/dmri.bedpostX/nodif_brain_mask.nii.gz')
+pbx2.inputs.omatrix2 = True
 pbx2.inputs.opd = True
 pbx2.inputs.os2t = True
 pbx2.inputs.loop_check = True
@@ -48,26 +55,22 @@ datasink.inputs.base_directory = os.path.abspath('/om/scratch/Fri/ksitek/probtra
 
 tractography = pe.Workflow(name='tractography')
 tractography.connect([
-    (infosource, datasource, [('subject_id', 'subject_id')],),
-    (datasource, binarize, [('aparc','in_file'),
-                            ('thsamples', 'thsamples'),
-                            ('phsamples', 'phsamples'),
-                            ('fsamples', 'fsamples') ])])
-tractography.connect([(binarize, probtrackx, [('binary_file', 'seed')]
-tractography.connect([(datasource, probtrackx,
+    (infosource, datasource, [('subject_id', 'subject_id')])])
+tractography.connect([(binarize, pbx2, [('binary_file', 'seed')] )])
+tractography.connect([(datasource, pbx2,
                       [('thsamples', 'thsamples'),
                        ('phsamples', 'phsamples'),
                        ('fsamples', 'fsamples'),
-                       ('target_masks', 'mask'),
-                       ('aseg','aseg') ])])
+                       ('target_masks', 'mask')])])
+tractography.connect([(datasource, binarize, [('aseg','in_file')] )])
 tractography.connect([(infosource, datasink, [('subject_id', 'container')]),
-                      (probtrackx, datasink,
-                      [('probtrackx.fdt_paths','probtrackx.@fdt_paths'),
-                       ('probtrackx.log','probtrackx.@log'),
-                       ('probtrackx.lookup_tractspace','probtrackx.@lookup_tractspace'),
-                       ('probtrackx.matrix2_dot','probtrackx.@matrix2_dot'),
-                       ('probtrackx.targets','probtrackx.@targets'),
-                       ('probtrackx.way_total','probtrackx.@way_total') ) ])
+                      (pbx2, datasink,
+                      [('fdt_paths','probtrackx.@fdt_paths'),
+                       ('log','probtrackx.@log'),
+                       ('lookup_tractspace','probtrackx.@lookup_tractspace'),
+                       ('matrix2_dot','probtrackx.@matrix2_dot'),
+                       ('targets','probtrackx.@targets'),
+                       ('way_total','probtrackx.@way_total') ]) ])
 
 tractography.run('SLURM',
                  plugin_args={'sbatch_args': '--qos=gablab --time=18:00:00 -N1 -c2 --mem=40G'})
